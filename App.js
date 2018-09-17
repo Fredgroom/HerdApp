8dc82de5be657d8cf7232ec2c6a97cda4e0243c7
Matt Calthrop <mcalthrop@gmail.com>
2018-09-17 16:54:25 +0100

    Add prettier (#4)

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
