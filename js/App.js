8a19a3f25b3dfd9ce4c38c036f8cf5dcb722995a
Matt Calthrop <matt.calthrop@redacademy.com>
2018-09-25 18:07:31 +0100

    Remove unused code in App.js

--- js/App.js
+++ js/App.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
 import RootStack from './navigation/RootStackNavigator';
+
 export default class App extends Component {
   render() {
     return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
