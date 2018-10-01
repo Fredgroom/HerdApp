792d7872d54ac9eda86f3a70dcc665bd035e0851
Freddie <freddie@freddiegroom.com>
2018-10-01 13:00:58 +0100

    feed screen styled (#59)
    * feed screen styled
    
    * removed arrowbrackets for more...
    
    * changed name of arrow class
    
    * android styling using dimension

--- js/screens/Feed/Feed.js
+++ js/screens/Feed/Feed.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { ScrollView, View, Text, TouchableHighlight } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { styles } from './styles';
 import Promo from '../../components/Promo';
@@ -9,15 +9,15 @@ import Venue from '../../components/Venue';
 
 const Feed = ({ navigation, promoData, eventData, venueData }) => {
   return (
-    <View>
-      <Text>
+    <ScrollView>
+      <Text style={styles.headingText}>
         Your guide to all local events and promotions at venues near you.
       </Text>
       <View>
         <View style={styles.feedHeaderText}>
-          <Text>EXCLUSIVE HERD OFFERS</Text>
+          <Text style={styles.componentTitle}>EXCLUSIVE HERD OFFERS</Text>
           <TouchableHighlight onPress={() => navigation.navigate('AllPromos')}>
-            <Text>more...</Text>
+            <Text style={styles.moreLink}>MORE...</Text>
           </TouchableHighlight>
         </View>
         <Promo promoData={promoData} />
@@ -25,9 +25,9 @@ const Feed = ({ navigation, promoData, eventData, venueData }) => {
 
       <View>
         <View style={styles.feedHeaderText}>
-          <Text>LOCAL EVENTS FOR YOU</Text>
+          <Text style={styles.componentTitle}>LOCAL EVENTS FOR YOU</Text>
           <TouchableHighlight onPress={() => navigation.navigate('AllEvents')}>
-            <Text>more...</Text>
+            <Text style={styles.moreLink}>MORE...</Text>
           </TouchableHighlight>
         </View>
         <Event eventData={eventData} />
@@ -35,14 +35,14 @@ const Feed = ({ navigation, promoData, eventData, venueData }) => {
 
       <View>
         <View style={styles.feedHeaderText}>
-          <Text>YOUR LOCAL VENUES</Text>
+          <Text style={styles.componentTitle}>YOUR LOCAL VENUES</Text>
           <TouchableHighlight onPress={() => navigation.navigate('AllVenues')}>
-            <Text>more...</Text>
+            <Text style={styles.moreLink}>MORE...</Text>
           </TouchableHighlight>
         </View>
         <Venue venueData={venueData} />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- js/screens/Feed/styles.js
+++ js/screens/Feed/styles.js
@@ -1,8 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
   feedHeaderText: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  headingText: {
+    margin: width * 0.1,
+    fontSize: height * 0.03,
+    textAlign: 'center',
+  },
+  componentTitle: {
+    fontSize: height * 0.02,
+    marginLeft: width * 0.03,
+  },
+  moreLink: {
+    fontSize: height * 0.02,
+    marginRight: width * 0.03,
   },
 });
