0bcc056cae17baaaedcfc6b07372d480f9584921
Darragh McAlone <32821836+Darraghmcalone@users.noreply.github.com>
2018-10-05 11:48:41 +0100

    insert image bacground and style transparent text link (#79)
    * insert image bacground and style transparent text link
    
    * removed react final form; grouped external libraries together;
    
    * fixed styling imagebackground for android

--- js/screens/LogIn/LogIn.js
+++ js/screens/LogIn/LogIn.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { View, Button, AsyncStorage } from 'react-native';
+import { View, Text, ImageBackground, AsyncStorage } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { styles } from './styles';
 
 class LogIn extends Component {
   render() {
     return (
-      <View>
-        <Button title="Sign in!" onPress={this._signInAsync} />
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require('../../assets/images/staticPages/registration_screen.png')}
+          resizeMode="contain"
+        >
+          <View>
+            <Text style={styles.mainAppLink} onPress={this._signInAsync} />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 
   _signInAsync = async () => {
-    await AsyncStorage.setItem('userToken', 'abc');
+    await AsyncStorage.setItem('userToken', 'logged-in');
     this.props.navigation.navigate('MainApp');
   };
 }
---
+++ js/screens/LogIn/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+  },
+  backgroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  mainAppLink: {
+    width: '90%',
+    height: '35%',
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    marginTop: '80%',
+    marginBottom: '30%',
+    alignSelf: 'center',
+  },
+});
